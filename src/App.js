e80f9185aee108536b0bdf6735dad5c37a29b169
ihossen016 <ihossen016@gmail.com>
2022-04-05 10:12:37 +0600

    card disable feat added

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   const [turns, setTurns] = useState(0);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 
   // Shuffle Cards
   const shuffleCards = () => {
@@ -35,6 +36,9 @@ function App() {
   // Compare 2 Selected Cards
   useEffect(() => {
     if (choiceOne && choiceTwo) {
+      // Disabling cards
+      setDisabled(true);
+
       if (choiceOne.src === choiceTwo.src) {
         setCards(prevCards => {
           return prevCards.map(card => {
@@ -61,6 +65,7 @@ function App() {
     setChoiceOne(null);
     setChoiceTwo(null);
     setTurns(prevTurns => prevTurns + 1);
+    setDisabled(false);
   };
 
   return (
@@ -75,6 +80,7 @@ function App() {
             card={card}
             handleChoice={handleChoice}
             flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
           />
         ))}
       </div>
