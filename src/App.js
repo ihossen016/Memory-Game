3f6f7f0eea2181d8142ea3b922041a3d0999683b
ihossen016 <ihossen016@gmail.com>
2022-04-05 10:19:09 +0600

    turn count added & game start automatically feat added

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ function App() {
       .sort(() => Math.random() - 0.5)
       .map(card => ({ ...card, id: Math.random() }));
 
+    setChoiceOne(null);
+    setChoiceTwo(null);
     setCards(shuffledCards);
     setTurns(0);
   };
@@ -58,8 +60,6 @@ function App() {
     }
   }, [choiceOne, choiceTwo]);
 
-  console.log(cards);
-
   // Reset Choices & increase Turns
   const resetTurn = () => {
     setChoiceOne(null);
@@ -67,6 +67,11 @@ function App() {
     setTurns(prevTurns => prevTurns + 1);
     setDisabled(false);
   };
+
+  // Start the game automatically
+  useEffect(() => {
+    shuffleCards();
+  }, []);
 
   return (
     <div className="App">
@@ -84,6 +89,7 @@ function App() {
           />
         ))}
       </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
